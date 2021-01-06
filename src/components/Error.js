b68a12943aff84a9b0084d4fa702a01e88939c80
pjh37 <37110261+pjh37@users.noreply.github.com>
2021-01-07 03:01:17 +0900

    feat: error handle page

---
+++ src/components/Error.js
@@ -0,0 +1,13 @@
+import {useEffect} from 'react';
+
+function Error() {
+    return (
+        <div class="container">
+            <h2>
+                Error!! 새로고침 혹은 재접속 해주세요.
+            </h2>
+        </div>
+    )
+}
+
+export default Error
