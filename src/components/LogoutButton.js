78a5a9c9a886c842fd290e8c6be8e7bb5deb3c09
pjh37 <37110261+pjh37@users.noreply.github.com>
2021-01-07 03:07:42 +0900

    fix: 로그아웃할 경우 관련정보 localStorage clear

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import {LOGOUT_URL} from '../constants/index'
+import '../css/Header.css';
 
 function LogoutButton(props) {
   const handleClick = () => {
     console.log(localStorage.getItem('username'))
-    props.history.push('/');
+    localStorage.removeItem('username')
+    
   }
-  return <button class='btn btn-secondary' onClick={handleClick}>{localStorage.getItem('username')}</button>;
+  return <a href={LOGOUT_URL} onClick={handleClick} id="logout">로그아웃</a>;
 }
 
 export default withRouter(LogoutButton);
