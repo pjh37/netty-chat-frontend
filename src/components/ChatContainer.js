5d4307eaaffc68e442c2b5d83a1326928c73cb77
pjh37 <37110261+pjh37@users.noreply.github.com>
2021-01-07 02:59:55 +0900

    fix: 웹소켓 파일 분리

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,134 +1,28 @@
 import React,{Component,useEffect,useState} from 'react';
+import Loading from '../components/Loading';
 import '../css/ChatContainer.css'; 
+import {myWebsocket} from '../util/WebSocket.js';
 
-
-const uri = "ws://localhost:8081/ws";
-const websocket = new WebSocket(uri);
 var fetching=false;
-const myWebsocket={
-    roomId:'',
-    init:function(id){
-        if(id==null)return
-        var _this=this;
-        _this.roomId=id
-        
-        document.querySelector('#btn-send').addEventListener('click',(e)=>{
-            _this.onSend();
-            console.log('send clicked') 
-        })
-        document.querySelector('#message-input').addEventListener('keydown',(e)=>{
-            if(e.keyCode===13){
-                _this.onSend();
-                console.log('enter') 
-            }
-        })
-        
-        
-        _this.connect(id);
-    },
-    connect:function(id){
-        var _this=this;
-        if(websocket.readyState===WebSocket.OPEN){
-            console.log("웹소켓 열려있음")
-            _this.roomEnter(id);
-            return
-        }else{
-            console.log("웹소켓 닫혀있음")
-            websocket.onopen = (e)=> {
-                console.log("open")
-                _this.roomEnter(id);
-            };
-        }
-        websocket.onmessage=(e)=>{
-            console.log("메세지 받음")
-            _this.onReceive(e)
-        }
-        websocket.onclose=(e)=>{
-            alert('연결종료')
-        }
-    },
-    roomEnter:function(roomId){
-        
-        var msg={
-            command:"ROOM_ENTER",
-            body:{
-                roomId: roomId,
-                username:'홍길동'
-            }
-        }
-        console.log('roomEnter: '+roomId)
-        websocket.send(JSON.stringify(msg));
-    },
-    onSend: function () {
-        var message = document.querySelector('#message-input').value;
-        var _this=this;
-        var msg = {
-            command: "CHAT_LOG_RECEIVE",
-            body: {
-                username: document.querySelector('#username').value,
-                content: message,
-                roomId: _this.roomId
-            }
-        }
-        document.querySelector('#message-input').value = '';
-        console.log(JSON.stringify(msg));
-        websocket.send(JSON.stringify(msg));
-    },
-    onReceive: function (e) {
-        var _this=this;
-        let data = JSON.parse(e.data);
-        if(data.roomId!=_this.roomId){
-            return
-        }
-        let messageElem = document.createElement('div');
-        let nicNameElem = document.createElement('div');
-        nicNameElem.textContent = data.username;
-
-
-        messageElem.style.width = "600px";
-        messageElem.className = "card w-75";
-        messageElem.textContent = data.content;
-        messageElem.style.wordBreak = "keep-all";
-        messageElem.style.wordWrap = "break-word";
-        messageElem.style.marginTop = "10px";
-        messageElem.style.marginLeft = "10px";
-        if (document.querySelector('#username').value == data.username) {
-            nicNameElem.style.color = "#736b5e";
-            messageElem.style.background = '#e8e6e3';
-            messageElem.style.color = 'black';
-        } else {
-            nicNameElem.style.color = "white";
-            messageElem.style.background = '#0d6efd';
-            messageElem.style.color = 'white';
-        }
-        messageElem.prepend(nicNameElem);
-        document.querySelector('.chat-message-container').append(messageElem)
-    }
-}
 
 const ChatItem=({item})=>(
-    document.querySelector('#username').value == item.name ? (
+    localStorage.getItem('username') == item.username ? (
         <div class="card w-75 my-message message">
             <div class="my-name">
-                {item.name}
+                {item.username}
             </div>
             {item.content}
         </div>
     ):(
         <div class="card w-75 your-message message">
             <div class="your-name">
-                {item.name}
+                {item.username}
             </div>
             {item.content}
         </div>
     )
-    
 )
-const Spinner = () => (
-    <div class="spinner-border text-success" role="status" id="loading">
-        <span class="sr-only">Loading...</span>
-    </div>
-)
+
 function ChatContainer({match}){
     const [roomId,setRoomId]=useState(null)
     const [page,setPage]=useState(1)
@@ -144,7 +38,7 @@ function ChatContainer({match}){
             setChatlist(mergedData)
             document.querySelector('.chat-message-container').scrollTo(0,
                 document.querySelector('.chat-message-container').clientHeight)
-            fetching=false
+                fetching=false
             setLoading(false)
         })
     }
@@ -200,12 +94,12 @@ function ChatContainer({match}){
                 <div class="chat-message-container card w-75" onScroll={handleScroll}>
                     
                     {
-                        chatlist==null ? (<Spinner></Spinner>):chatlist.map(chatItem=>(
+                        chatlist==null ? (<Loading/>):chatlist.map(chatItem=>(
                             <ChatItem key={chatItem.id} item={chatItem}></ChatItem>
                         ))
                     }
                     {
-                        loading==true ? (<Spinner></Spinner>):('')
+                        loading==true ? (<Loading/>):('')
                     }
                 </div>
                 <input type="text" class="form-control w-75" id="username" placeholder="닉네임" aria-label="username"
---
+++ src/util/WebSocket.js
@@ -0,0 +1,116 @@
+export const uri = "ws://localhost:8081/ws";
+export const websocket = new WebSocket(uri);
+export const myWebsocket={
+    roomId:'',
+    init:function(id){
+        if(id==null)return
+        var _this=this;
+        _this.roomId=id
+        
+        document.querySelector('#btn-send').addEventListener('click',(e)=>{
+            _this.onSend();
+            console.log('send clicked') 
+        })
+        document.querySelector('#message-input').addEventListener('keydown',(e)=>{
+            if(e.keyCode===13){
+                _this.onSend();
+                console.log('enter') 
+            }
+        })
+        
+        
+        _this.connect(id);
+    },
+    connect:function(id){
+        var _this=this;
+        if(websocket.readyState===WebSocket.OPEN){
+            console.log("웹소켓 열려있음")
+            _this.roomEnter(id);
+            return
+        }else{
+            console.log("웹소켓 닫혀있음")
+            websocket.onopen = (e)=> {
+                console.log("open")
+                _this.roomEnter(id);
+            };
+        }
+        websocket.onmessage=(e)=>{
+            console.log("메세지 받음")
+            _this.onReceive(e)
+        }
+        websocket.onclose=(e)=>{
+            alert('연결종료')
+        }
+    },
+    roomEnter:function(roomId){
+        
+        var msg={
+            command:"ROOM_ENTER",
+            body:{
+                roomId: roomId,
+                username:'홍길동'
+            }
+        }
+        console.log('roomEnter: '+roomId)
+        websocket.send(JSON.stringify(msg));
+    },
+    roomCreate:function(){
+        var name = localStorage.getItem('username');
+        var msg = {
+            command: "ROOM_CREATE",
+            body: {
+                username: name,
+                roomName: document.querySelector('#room-name').value
+            }
+        }
+        websocket.send(JSON.stringify(msg));
+        window.location.reload();
+        document.querySelector('#roomCreateModal').modal('hide')
+        
+    },
+    onSend: function () {
+        var message = document.querySelector('#message-input').value;
+        var _this=this;
+        var msg = {
+            command: "CHAT_LOG_RECEIVE",
+            body: {
+                username: document.querySelector('#username').value,
+                content: message,
+                roomId: _this.roomId
+            }
+        }
+        document.querySelector('#message-input').value = '';
+        console.log(JSON.stringify(msg));
+        websocket.send(JSON.stringify(msg));
+    },
+    onReceive: function (e) {
+        var _this=this;
+        let data = JSON.parse(e.data);
+        if(data.roomId!=_this.roomId){
+            return
+        }
+        let messageElem = document.createElement('div');
+        let nicNameElem = document.createElement('div');
+        nicNameElem.textContent = data.username;
+
+
+        messageElem.style.width = "600px";
+        messageElem.className = "card w-75";
+        messageElem.textContent = data.content;
+        messageElem.style.wordBreak = "keep-all";
+        messageElem.style.wordWrap = "break-word";
+        messageElem.style.marginTop = "10px";
+        messageElem.style.marginLeft = "10px";
+        if (document.querySelector('#username').value == data.username) {
+            nicNameElem.style.color = "#736b5e";
+            messageElem.style.background = '#e8e6e3';
+            messageElem.style.color = 'black';
+        } else {
+            nicNameElem.style.color = "white";
+            messageElem.style.background = '#0d6efd';
+            messageElem.style.color = 'white';
+        }
+        messageElem.prepend(nicNameElem);
+        document.querySelector('.chat-message-container').append(messageElem)
+    }
+}
