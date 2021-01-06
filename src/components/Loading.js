7d157c9f4a79beea36ca37cfb17ff6d7c3017baf
pjh37 <37110261+pjh37@users.noreply.github.com>
2021-01-07 03:03:31 +0900

    feat: loading view 분리

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+function Loading() {
+    return (
+        <div class="spinner-border text-success" role="status" id="loading">
+            <span class="sr-only">Loading...</span>
+        </div>
+    )
+}
+
+export default Loading
