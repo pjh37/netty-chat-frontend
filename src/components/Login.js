27cc9b0c7031a355c7d68a7c0b0f98e3ebab9ff6
pjh37 <37110261+pjh37@users.noreply.github.com>
2021-01-07 03:06:41 +0900

    fix: class -> functional구조로 변경

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,35 +7,34 @@ import SignUp from './SignUp';
 import "../css/Login.css";
 
 
-class Login extends Component{
-    render() {
-        return (
-            <div class="container login-container">
-                <form action="http://localhost:8080/login" method="post">
-                    <div class="mb-3">
-                        <input type="text" class="form-control form-control-lg login-form" id="email" name="email" placeholder="이메일을 입력하세요." />
-                        <input type="password" class="form-control form-control-lg login-form" id="pw" name="password" placeholder="비밀번호를 입력하세요." />
-                    </div>
-                    <div class="row-3">
-                        <button type="submit" class="btn btn-primary" id="btn-login">로그인</button>
-                    </div>
-                </form>
+function Login(props){
+    
+    return (
+        <div class="container login-container">
+            <form action="http://localhost:8080/login" method="post">
+                <div class="mb-3">
+                    <input type="text" class="form-control form-control-lg login-form" id="email" name="email" placeholder="이메일을 입력하세요." />
+                    <input type="password" class="form-control form-control-lg login-form" id="pw" name="password" placeholder="비밀번호를 입력하세요." />
+                </div>
                 <div class="row-3">
-                    <Link to={'/signup'}>
-                        <button class="btn btn-success" id="btn-signIn">회원가입</button>
-                    </Link>
-                    
+                    <button type="submit" class="btn btn-primary" id="btn-login">로그인</button>
                 </div>
-                <div class="row-3" id="social-login">
-                    <a href={GITHUB_AUTH_URL}>
-                        <GithubLoginButton />
-                    </a>
-                    
-                </div>
+            </form>
+            <div class="row-3">
+                <Link to={'/signup'}>
+                    <button class="btn btn-success" id="btn-signIn">회원가입</button>
+                </Link>
+                
             </div>
+            <div class="row-3" id="social-login">
+                <a href={GITHUB_AUTH_URL}>
+                    <GithubLoginButton />
+                </a>
+                
+            </div>
+        </div>
 
-        );
-    }
+    )
 }
 
 export default Login
