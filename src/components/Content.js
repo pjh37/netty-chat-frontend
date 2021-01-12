164445448d963535da911566e4fdc156c314b036
pjh37 <37110261+pjh37@users.noreply.github.com>
2021-01-13 02:33:33 +0900

    fix: main content ui변동에 따른 수정

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
-import {Route,Router,BrowserRouter,Switch} from 'react-router-dom';
+import {Route,BrowserRouter as Router,Switch,Link} from 'react-router-dom';
+import '../css/Content.css';  
 import MenuContainer from './MenuContainer';
 import RoomListContainer from './RoomListContainer';
 import PagingList from './PagingList';
@@ -52,16 +53,44 @@ class Content extends Component{
     render(){
         return(
             <div class="container">
-                <MenuContainer></MenuContainer>
-                {
-                    this.state.content.data==null ? (<Loading/>) :('')
-                }
-                <Switch>
-                    <Route exact path="/">
-                        <RoomListContainer rooms={this.state.content.data}></RoomListContainer>
-                        <PagingList pagingList={this.state.content.pageList} pageItemClicked={this.pageItemClick}></PagingList>
-                    </Route>
-                </Switch>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <Link to={"/job"} class="card content-item">
+                            <div class="card-body">
+                                <h5 class="card-title">채용사이트 모음</h5>
+                                <p class="card-text">개발자들이 주로보는 채용사이트 모음입니다.</p>
+
+                            </div>
+                        </Link>
+                    </div>
+                    <div class="col-sm-6">
+                        <Link to={"/algorithm"} class="card content-item">
+                            <div class="card-body">
+                                <h5 class="card-title">알고리즘 문제 사이트 모음</h5>
+                                <p class="card-text">실제 채용때 코딩테스트 플랫폼으로 이용되는 사이트및 문제를 풀 수 있는 사이트 모음</p>
+                                
+                            </div>
+                        </Link>
+                    </div>
+                    <div class="col-sm-6">
+                        <Link to={"/realtimechat"} class="card content-item">
+                            <div class="card-body">
+                                <h5 class="card-title">채팅서비스</h5>
+                                <p class="card-text">유저들끼리 방을 만들어 실시간 채팅을 할 수 있으며 URL만을 공유해 원하는 사람들끼리만 채팅할 수 있습니다.</p>
+                                
+                            </div>
+                        </Link>
+                    </div>
+                    <div class="col-sm-6">
+                        <Link to={"/board"} class="card content-item">
+                            <div class="card-body">
+                                <h5 class="card-title">게시판</h5>
+                                <p class="card-text">궁금한점 혹은 Q/A등은 이곳에서 해주세요</p>
+                                
+                            </div>
+                        </Link>
+                    </div>
+                </div>
             </div>
         );
     }
